af8f0ae16ae36797d16ae979954c3d8ad57116d7
Kevin.K <102898544+KevinKalt0@users.noreply.github.com>
2023-07-09 23:46:33 +0200

    Add files via upload

---
+++ premier-projet-sb/src/main/java/fr/sainsaspais/l3/demospringboot/PremierProjetSbApplication.java
@@ -0,0 +1,13 @@
+package fr.sainsaspais.l3.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierProjetSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierProjetSbApplication.class, args);
+	}
+
+}
---
+++ premier-projet-sb/src/test/java/fr/sainsaspais/l3/demospringboot/PremierProjetSbApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sainsaspais.l3.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierProjetSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
